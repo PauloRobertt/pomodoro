a640a7f03773e6f99bac5b9231778500d52a9e49
paulorobertt <paulorobert5776@gmail.com>
2026-01-30 17:54:45 -0300

    Mudança no nome da classe Timer

--- pomodoro/src/class/Timer.ts
+++ pomodoro/src/class/Timer.ts
@@ -3,29 +3,37 @@ export class Timer {
   private segundos: number = 0;
   private intervalID: any = null;
 
-  constructor(private onTick: (min: number, sec: number) => void) {}
+  constructor(
+    private onTick: (min: number, sec: number) => void,
+    public focus: number = 25,
+    public shortBreak: number = 5,
+    public longBreak: number = 15,
+  ) {
+    this.minutos = this.focus;
+  }
 
   startTime() {
     if (this.intervalID) return;
 
     this.intervalID = setInterval(() => {
-      if (this.segundos >= 59) {
-        this.segundos = 0;
-        this.minutos++;
+      if (this.segundos <= 0) {
+        this.segundos = 60;
+        this.minutos--;
       }
 
-      this.segundos++;
+      this.segundos--;
       this.onTick(this.minutos, this.segundos);
-    }, 1000);
+    }, 300);
   }
 
   stopTimer() {
+    console.log(this.minutos);
     clearInterval(this.intervalID);
     this.intervalID = null;
   }
 
   resetTimer() {
-    this.minutos = 0;
+    this.minutos = this.focus;
     this.segundos = 0;
     this.stopTimer();
     this.onTick(this.minutos, this.segundos);
