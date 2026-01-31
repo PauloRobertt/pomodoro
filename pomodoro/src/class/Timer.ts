986c400e3ba0e3e25ede4cbf14f92b260aaaa3f1
paulorobertt <paulorobert5776@gmail.com>
2026-01-31 19:06:47 -0300

    Aprimoramento o codigo, para obter apenas os segundos e transforma em minutos ou horas

--- pomodoro/src/class/Timer.ts
+++ pomodoro/src/class/Timer.ts
@@ -1,20 +1,32 @@
 export class Timer {
+  private timer: number = 0;
+
+  private horas: number = 0;
   private minutos: number = 0;
   private segundos: number = 0;
+
+  private ciclos: number = 0;
   private intervalID: any = null;
 
   constructor(
-    private onTick: (min: number, sec: number) => void,
+    private onTick: (hors: number, min: number, sec: number) => void,
     public focus: number = 25,
     public shortBreak: number = 5,
     public longBreak: number = 15,
+    public cycle: number = 4,
   ) {
-    this.minutos = this.focus;
+    this.minutos = this.focus / 60;
+
+    if (this.minutos > 60) {
+      this.horas = Math.floor(this.minutos / 60);
+      this.minutos = 0;
+    }
+
+    this.onTick(this.horas, this.minutos, this.segundos);
   }
 
-  startTime() {
-    if (this.intervalID) return;
-
+  shortBreakTime() {
+    this.minutos = this.shortBreak;
     this.intervalID = setInterval(() => {
       if (this.segundos <= 0) {
         this.segundos = 60;
@@ -22,20 +34,72 @@ export class Timer {
       }
 
       this.segundos--;
+
+      if (this.minutos <= 0 && this.segundos <= 0) {
+        this.stopTimer();
+        this.startTime();
+      }
       this.onTick(this.minutos, this.segundos);
-    }, 300);
+    }, 100);
+  }
+
+  longBreakTime() {
+    this.minutos = this.longBreak;
+    this.intervalID = setInterval(() => {
+      if (this.segundos <= 0) {
+        this.segundos = 60;
+        this.minutos--;
+      }
+
+      this.segundos--;
+
+      if (this.minutos <= 0 && this.segundos <= 0) {
+        this.stopTimer();
+        this.startTime();
+        console.log("LongBreak Acabou");
+      }
+      this.onTick(this.minutos, this.segundos);
+    }, 100);
+  }
+
+  startTime() {
+    if (this.intervalID) return;
+
+    this.intervalID = setInterval(() => {
+      if (this.segundos <= 0) {
+        this.segundos = 59;
+        this.minutos--;
+      }
+
+      if (this.minutos <= 0 && this.horas > 0) {
+        this.minutos = 59;
+        this.horas--;
+      }
+
+      this.segundos--;
+
+      if (this.minutos <= 0 && this.segundos <= 0) {
+        this.stopTimer();
+      }
+
+      this.onTick(this.horas, this.minutos, this.segundos);
+    }, 1);
   }
 
   stopTimer() {
-    console.log(this.minutos);
     clearInterval(this.intervalID);
     this.intervalID = null;
   }
 
   resetTimer() {
-    this.minutos = this.focus;
-    this.segundos = 0;
     this.stopTimer();
-    this.onTick(this.minutos, this.segundos);
+    this.minutos = this.focus / 60;
+    this.segundos = 0;
+
+    if (this.minutos > 60) {
+      this.horas = Math.floor(this.minutos / 60);
+      this.minutos = 0;
+    }
+    this.onTick(this.horas, this.minutos, this.segundos);
   }
 }
