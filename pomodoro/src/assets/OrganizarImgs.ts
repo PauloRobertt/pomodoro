6dbfdf5ecfa8001627b606ff706e0c4093841305
paulorobertt <paulorobert5776@gmail.com>
2026-02-03 21:11:18 -0300

    Implementação de novas imagens e arquivo para organizar as imagens

---
+++ pomodoro/src/assets/OrganizarImgs.ts
@@ -0,0 +1,13 @@
+import pomodoroDark from "./imgs/pomodoroDark.png";
+import pomodoroDefault from "./imgs/pomodoroDefault.png";
+import pomodoroEmpty from "./imgs/pomodoroEmpty.png";
+import foco from "./imgs/foco.png";
+import config from "./imgs/config.png";
+
+export const OrganizarImgs = {
+  pomodoroDark: pomodoroDark,
+  pomodoroDefault: pomodoroDefault,
+  pomodoroEmpty: pomodoroEmpty,
+  foco: foco,
+  config: config,
+};
