09e1fcefb5bcd27e2c3b5f42b61d8f67606de958
paulorobertt <paulorobert5776@gmail.com>
2026-02-05 20:25:37 -0300

    Modificação para a utilização do menu com a função saveConfig e o armazenamento dos dados de tempo em useState

--- pomodoro/src/pages/pomodoro/pomodoro.tsx
+++ pomodoro/src/pages/pomodoro/pomodoro.tsx
@@ -5,8 +5,10 @@ import { Timer } from "../../class/Timer";
 import { OrganizarImgs } from "../../assets/OrganizarImgs.ts";
 
 import Button from "../../components/button/button.tsx";
+import Menu from "../../layout/menu/menu.tsx";
 
 import styles from "./pomodoro.module.css";
+import stylesButton from "../../components/button/button.module.css";
 
 export default function Pomodoro({
   focus,
@@ -14,6 +16,11 @@ export default function Pomodoro({
   longBreak,
   cycle,
 }: TimerProps) {
+  const [timeFocus, setTimeFocus] = useState(focus);
+  const [timeShort, setTimeShort] = useState(shortBreak);
+  const [timeLong, setTimeLong] = useState(longBreak);
+  const [timeCycle, setTimeCycle] = useState(cycle);
+
   const [statusTimer, setStatusTimer] = useState("");
   const [isUseTimer, setIsUseTimer] = useState(false);
   const [horas, setHoras] = useState(0);
@@ -30,89 +37,113 @@ export default function Pomodoro({
         setMinutos(min);
         setSegundos(sec);
       },
-      focus,
-      shortBreak,
-      longBreak,
-      cycle,
+      timeFocus,
+      timeShort,
+      timeLong,
+      timeCycle,
     );
-  }, []);
+  }, [timeFocus]);
+
+  function saveConfig(e: any) {
+    e.preventDefault();
+    const inputFocusValue = document.getElementById(
+      "inputFocus",
+    ) as HTMLInputElement;
+    const inputShortValue = document.getElementById(
+      "inputShort",
+    ) as HTMLInputElement;
+    const inputLongValue = document.getElementById(
+      "inputLong",
+    ) as HTMLInputElement;
+
+    setTimeFocus(Number(inputFocusValue.value) * 60);
+    setTimeShort(Number(inputShortValue.value) * 60);
+    setTimeLong(Number(inputLongValue.value) * 60);
+  }
 
   return (
-    <div className={styles.containerBackground}>
-      <div className={styles.containerPomodoro}>
-        <div className={styles.circleWrapper}>
-          <svg width="150" height="150" className={styles.circleContent}>
-            <circle
-              className={styles.circle}
-              r="130"
-              cx="150"
-              cy="150"
-              fill="white"
-              stroke="var(--secundaryColor)"
-              strokeWidth="5px"
-              strokeDasharray="816,4"
-            />
+    <div className={styles.containerPomodoro}>
+      <Menu
+        defaultValueFocus={timeFocus}
+        defaultValueShortBreak={timeShort}
+        defaultValueLongBreak={timeLong}
+        defualtValueCycle={timeCycle}
+        functionSaveConfig={saveConfig}
+      />
+      <div className={styles.circleWrapper}>
+        <svg width="150" height="150" className={styles.circleContent}>
+          <circle
+            className={styles.circle}
+            r="130"
+            cx="150"
+            cy="150"
+            fill="white"
+            stroke="var(--secundaryColor)"
+            strokeWidth="5px"
+            strokeDasharray="816,4"
+          />
 
-            <circle
-              className={styles.circle}
-              r="130"
-              cx="150"
-              cy="150"
-              fill="white"
-              stroke="var(--primaryColor)"
-              strokeWidth="5px"
-              strokeDasharray="816,4"
+          <circle
+            className={styles.circle}
+            r="130"
+            cx="150"
+            cy="150"
+            fill="white"
+            stroke="var(--primaryColor)"
+            strokeWidth="5px"
+            strokeDasharray="816,4"
+          />
+        </svg>
+        <div className={styles.containerTimer}>
+          <h1>
+            {horas > 0 ? <>{String(horas).padStart(2, "0")}:</> : <></>}
+            {String(minutos).padStart(2, "0")}:
+            {String(segundos).padStart(2, "0")}
+          </h1>
+          <small>{statusTimer}</small>
+          <small>
+            {Array.from({ length: cycle }, (_, index) => (
+              <img
+                className={styles.ciclosImg}
+                src={OrganizarImgs.pomodoroEmpty}
+                alt=""
+              />
+            ))}
+          </small>
+        </div>
+      </div>
+      <div className={styles.containerButtons}>
+        {isUseTimer ? (
+          <>
+            <Button
+              action={() => {
+                setIsUseTimer(false);
+                timerRef.current?.stopTimer();
+              }}
+              styleButton={stylesButton.mainButton}
+              text="Stop"
             />
-          </svg>
-          <div className={styles.containerTimer}>
-            <h1>
-              {horas > 0 ? <>{String(horas).padStart(2, "0")}:</> : <></>}
-              {String(minutos).padStart(2, "0")}:
-              {String(segundos).padStart(2, "0")}
-            </h1>
-            <small>{statusTimer}</small>
-            <small>
-              {Array.from({ length: cycle }, (_, index) => (
-                <img
-                  className={styles.ciclosImg}
-                  src={OrganizarImgs.pomodoroEmpty}
-                  alt=""
-                />
-              ))}
-            </small>
-          </div>
-        </div>
-
-        <div className={styles.containerButtons}>
-          {isUseTimer ? (
-            <>
-              <Button
-                action={() => {
-                  setIsUseTimer(false);
-                  timerRef.current?.stopTimer();
-                }}
-                text="Stop"
-              />
-              <Button
-                action={() => {
-                  setIsUseTimer(false);
-                  timerRef.current?.resetTimer();
-                }}
-                text="Reset"
-              />
-            </>
-          ) : (
-            <>
-              <Button
-                action={() => {
-                  setIsUseTimer(true);
-                  timerRef.current?.startTime();
-                }}
-                text="Start"
-              />
-            </>
-          )}
-        </div>
+            <Button
+              action={() => {
+                setIsUseTimer(false);
+                timerRef.current?.resetTimer();
+              }}
+              styleButton={stylesButton.mainButton}
+              text="Reset"
+            />
+          </>
+        ) : (
+          <>
+            <Button
+              action={() => {
+                setIsUseTimer(true);
+                timerRef.current?.startTime();
+              }}
+              styleButton={stylesButton.mainButton}
+              text="Start"
+            />
+          </>
+        )}
       </div>
     </div>
   );
