447bdc3ddebc27dc61d1a0d683c9cc63cd6481eb
paulorobertt <paulorobert5776@gmail.com>
2026-02-05 19:52:13 -0300

    Mundança de como o componente aparece na tela e modificação para que os segundos se transformem em minutos  no menu

--- pomodoro/src/layout/menu/menu.tsx
+++ pomodoro/src/layout/menu/menu.tsx
@@ -23,62 +23,62 @@ export default function Menu({
           alt="Icone configuração"
         />
       </figure>
-      {isShowMenu ? (
-        <div className={styles.containerConfig}>
-          <div className={styles.contentConfig}>
-            <div className={styles.opConfig}>
-              <div className={styles.op}>
-                Focus
-                <div className={styles.time}>
-                  <input
-                    type="number"
-                    id="inputFocus"
-                    name="valueFocus"
-                    defaultValue={defaultValueFocus}
-                  />
-                  min
-                </div>
-              </div>
-              <div className={styles.op}>
-                ShortBreak
-                <div className={styles.time}>
-                  <input
-                    type="number"
-                    id="inputShort"
-                    name="valueShort"
-                    defaultValue={defaultValueShortBreak}
-                  />
-                  min
-                </div>
-              </div>
-              <div className={styles.op}>
-                LongBreak
-                <div className={styles.time}>
-                  <input
-                    type="number"
-                    id="inputLong"
-                    name="valueLong"
-                    defaultValue={defaultValueLongBreak}
-                  />
-                  min
-                </div>
+      <div
+        className={
+          isShowMenu ? styles.containerConfigOpen : styles.containerConfig
+        }
+      >
+        <div className={styles.contentConfig}>
+          <div className={styles.opConfig}>
+            <div className={styles.op}>
+              Focus
+              <div className={styles.time}>
+                <input
+                  type="number"
+                  id="inputFocus"
+                  name="valueFocus"
+                  defaultValue={defaultValueFocus / 60}
+                />
+                min
               </div>
             </div>
-
-            <div className={styles.buttonsConfig}>
-              <Button
-                action={() => {
-                  setIsShowMenu((prev) => !prev);
-                }}
-                text="Cancel"
-              />
-              <Button action={functionSaveConfig} text="Save" />
+            <div className={styles.op}>
+              ShortBreak
+              <div className={styles.time}>
+                <input
+                  type="number"
+                  id="inputShort"
+                  name="valueShort"
+                  defaultValue={defaultValueShortBreak / 60}
+                />
+                min
+              </div>
+            </div>
+            <div className={styles.op}>
+              LongBreak
+              <div className={styles.time}>
+                <input
+                  type="number"
+                  id="inputLong"
+                  name="valueLong"
+                  defaultValue={defaultValueLongBreak / 60}
+                />
+                min
+              </div>
             </div>
           </div>
+
+          <div className={styles.buttonsConfig}>
+            <Button
+              action={() => {
+                setIsShowMenu((prev) => !prev);
+              }}
+              text="Cancel"
+            />
+            <Button action={functionSaveConfig} text="Save" />
+          </div>
         </div>
-      ) : (
-        ""
-      )}
+      </div>
     </div>
   );
 }
