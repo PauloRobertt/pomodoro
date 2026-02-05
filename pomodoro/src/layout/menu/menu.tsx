5cc2b2db2fba0c93a542b1fe4a98a62b7f334100
paulorobertt <paulorobert5776@gmail.com>
2026-02-04 22:26:40 -0300

    Implementação do layout  menu para facilitar a edição dos tempos

---
+++ pomodoro/src/layout/menu/menu.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import styles from "./menu.module.css";
+import type { MenuProps } from "../../types/menu";
+import { OrganizarImgs } from "../../assets/OrganizarImgs";
+import Button from "../../components/button/button";
+
+export default function Menu({
+  defaultValueFocus,
+  defaultValueShortBreak,
+  defaultValueLongBreak,
+  functionSaveConfig,
+}: MenuProps) {
+  const [isShowMenu, setIsShowMenu] = useState(false);
+
+  return (
+    <div className={styles.layoutMenu}>
+      <figure className={styles.imgConfig}>
+        <img
+          onClick={() => {
+            setIsShowMenu((prev) => !prev);
+          }}
+          src={OrganizarImgs.config}
+          alt="Icone configuração"
+        />
+      </figure>
+      {isShowMenu ? (
+        <div className={styles.containerConfig}>
+          <div className={styles.contentConfig}>
+            <div className={styles.opConfig}>
+              <div className={styles.op}>
+                Focus
+                <div className={styles.time}>
+                  <input
+                    type="number"
+                    id="inputFocus"
+                    name="valueFocus"
+                    defaultValue={defaultValueFocus}
+                  />
+                  min
+                </div>
+              </div>
+              <div className={styles.op}>
+                ShortBreak
+                <div className={styles.time}>
+                  <input
+                    type="number"
+                    id="inputShort"
+                    name="valueShort"
+                    defaultValue={defaultValueShortBreak}
+                  />
+                  min
+                </div>
+              </div>
+              <div className={styles.op}>
+                LongBreak
+                <div className={styles.time}>
+                  <input
+                    type="number"
+                    id="inputLong"
+                    name="valueLong"
+                    defaultValue={defaultValueLongBreak}
+                  />
+                  min
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.buttonsConfig}>
+              <Button
+                action={() => {
+                  setIsShowMenu((prev) => !prev);
+                }}
+                text="Cancel"
+              />
+              <Button action={functionSaveConfig} text="Save" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
---
+++ pomodoro/src/types/menu.ts
@@ -0,0 +1,7 @@
+export type MenuProps = {
+  defaultValueFocus: number;
+  defaultValueShortBreak: number;
+  defaultValueLongBreak: number;
+  defualtValueCycle: number;
+  functionSaveConfig: any;
+};
