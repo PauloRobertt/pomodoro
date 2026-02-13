f98fd9936a3e4090b1083b0801bb2ec55ca29073
paulorobertt <paulorobert5776@gmail.com>
2026-02-13 20:34:50 -0300

    Mudança no design do menu

--- pomodoro/src/layout/menu/menu.tsx
+++ pomodoro/src/layout/menu/menu.tsx
@@ -1,9 +1,19 @@
+//React
 import { useState } from "react";
+
+//Styles
 import styles from "./menu.module.css";
 import stylesButton from "../../components/button/button.module.css";
+
+//types
 import type { MenuProps } from "../../types/menu";
+
+//Assets
 import { OrganizarImgs } from "../../assets/OrganizarImgs";
+
+//Componentes
 import Button from "../../components/button/button";
+import OptionMenu from "../../components/optionMenu/optionMenu";
 
 export default function Menu({
   defaultValueFocus,
@@ -13,6 +23,19 @@ export default function Menu({
   functionSaveConfig,
 }: MenuProps) {
   const [isShowMenu, setIsShowMenu] = useState(false);
+
+  const [valueFocus, setValueFocus] = useState(defaultValueFocus);
+  const [valueShort, setValueShort] = useState(defaultValueShortBreak);
+  const [valueLong, setValueLong] = useState(defaultValueLongBreak);
+  const [valueCycle, setValueCycle] = useState(defaultValueCycle);
+
+  const teste = () => {
+    setValueFocus(1);
+    console.log("Focus", valueFocus);
+    console.log("Short", valueShort);
+    console.log("Long", valueLong);
+    console.log("Cycle", valueCycle);
+  };
 
   return (
     <div className={styles.layoutMenu}>
@@ -32,50 +55,133 @@ export default function Menu({
       >
         <div className={styles.contentConfig}>
           <div className={styles.opConfig}>
-            <div className={styles.op}>
+            <div className={styles.containerOption}>
               Focus
-              <div className={styles.time}>
-                <input
-                  type="number"
-                  id="inputFocus"
-                  name="valueFocus"
-                  defaultValue={defaultValueFocus / 60}
+              <div className={styles.containerTimer}>
+                <Button
+                  text="-"
+                  action={() => {
+                    setValueFocus((prev) => {
+                      return (prev -= 300);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
                 />
-                min
+                <div className={styles.time}>
+                  <input
+                    type="number"
+                    id="inputFocus"
+                    name="valueFocus"
+                    value={valueFocus / 60}
+                  />
+                  min
+                </div>
+                <Button
+                  text="+"
+                  action={() => {
+                    setValueFocus((prev) => {
+                      return (prev += 300);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
+                />
               </div>
             </div>
-            <div className={styles.op}>
-              ShortBreak
-              <div className={styles.time}>
-                <input
-                  type="number"
-                  id="inputShort"
-                  name="valueShort"
-                  defaultValue={defaultValueShortBreak / 60}
+
+            <div className={styles.containerOption}>
+              ShortBreak Duration
+              <div className={styles.containerTimer}>
+                <Button
+                  text="-"
+                  action={() => {
+                    setValueShort((prev) => {
+                      return (prev -= 300);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
                 />
-                min
+                <div className={styles.time}>
+                  <input
+                    type="number"
+                    id="inputShort"
+                    name="valueShort"
+                    value={valueShort / 60}
+                  />
+                  min
+                </div>
+                <Button
+                  text="+"
+                  action={() => {
+                    setValueShort((prev) => {
+                      return (prev += 300);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
+                />
               </div>
             </div>
-            <div className={styles.op}>
-              LongBreak
-              <div className={styles.time}>
-                <input
-                  type="number"
-                  id="inputLong"
-                  name="valueLong"
-                  defaultValue={defaultValueLongBreak / 60}
+
+            <div className={styles.containerOption}>
+              LongBreak Duration
+              <div className={styles.containerTimer}>
+                <Button
+                  text="-"
+                  action={() => {
+                    setValueLong((prev) => {
+                      return (prev -= 300);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
                 />
-                min
+                <div className={styles.time}>
+                  <input
+                    type="number"
+                    id="inputLong"
+                    name="valueLong"
+                    value={valueLong / 60}
+                  />
+                  min
+                </div>
+                <Button
+                  text="+"
+                  action={() => {
+                    setValueLong((prev) => {
+                      return (prev += 300);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
+                />
               </div>
             </div>
-            <div className={styles.op}>
-              Cycle
-              <div className={styles.time}>
-                <input
-                  type="number"
-                  id="inputCycle"
-                  name="valueCycle"
-                  defaultValue={defaultValueCycle}
+
+            <div className={styles.containerOption}>
+              Cycles
+              <div className={styles.containerTimer}>
+                <Button
+                  text="-"
+                  action={() => {
+                    setValueCycle((prev) => {
+                      return (prev -= 1);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
+                />
+                <div className={styles.time}>
+                  <input
+                    type="number"
+                    id="inputCycle"
+                    name="valueCycle"
+                    value={valueCycle}
+                  />
+                </div>
+                <Button
+                  text="+"
+                  action={() => {
+                    setValueCycle((prev) => {
+                      return (prev += 1);
+                    });
+                  }}
+                  styleButton={stylesButton.buttonTimeControl}
                 />
               </div>
             </div>
