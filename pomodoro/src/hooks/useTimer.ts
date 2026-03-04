c7ec5f82283a878dfc0ed9f5d556019003bc746f
paulorobertt <paulorobert5776@gmail.com>
2026-03-04 01:01:16 -0300

    Implementação de nova verificação para caso todos os valores sejam zero e implementação do shortBreakTime para o hook

--- pomodoro/src/hooks/useTimer.ts
+++ pomodoro/src/hooks/useTimer.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import type { useTimer } from "../types/useTimer";
 
@@ -9,6 +9,8 @@ export function useTimer(props: useTimer) {
   const [cycle, setCycle] = useState(props.cycle);
 
   const [focusSegundos, setFocusSegundos] = useState(focus);
+  const [shortSegundos, setShortSegundos] = useState(shortBreak);
+
   const [tempoTotal, setTempoTotal] = useState(0);
   const [timeBreak, setTimeBreak] = useState(0);
   const [ciclosConcluidos, setCiclosConcluidos] = useState(0);
@@ -21,34 +23,41 @@ export function useTimer(props: useTimer) {
   const [intervalID, setIntervalID] = useState<number | undefined>(undefined);
 
   const timerFormat = (
-    value: number,
+    valueSeconds: number,
     tempoBreak: number,
     statusTimer: string,
   ) => {
     setTimeBreak(tempoBreak);
     setStatusTimer(statusTimer);
 
-    const horasCalculadas = Math.floor(value / 3600);
-    const minutosCalculados = Math.floor(value / 60);
-    const segundosCalculados = value % 60;
+    const horasCalculadas = Math.floor(valueSeconds / 3600);
+    const minutosCalculados = Math.floor(valueSeconds / 60);
+    const segundosCalculados = valueSeconds % 60;
 
     setHoras(horasCalculadas);
     setMinutos(minutosCalculados);
     setSegundos(segundosCalculados);
   };
 
-  const startTime = () => {
-    if (intervalID) return;
+  const focusTime = () => {
+    timerFormat(focusSegundos, focus, "Focus");
 
-    timerFormat(focusSegundos, timeBreak, "Focus");
-
-    var horasCalculadas = horas;
-    var minutosCalculados = minutos;
-    var segundosCalculados = segundos;
+    var horasCalculadas = Math.floor(focusSegundos / 3600);
+    var minutosCalculados = Math.floor(focusSegundos / 60);
+    var segundosCalculados = focusSegundos % 60;
+    var tempoTotalCalculado = tempoTotal;
     var teste = focusSegundos;
-    var ciclos = ciclosConcluidos;
 
-    var intervalIDTest = setInterval(() => {
+    var intervalIDFocus = setInterval(() => {
+      if (
+        !(horasCalculadas > 0) &&
+        !(minutosCalculados > 0) &&
+        !(segundosCalculados > 0)
+      ) {
+        stopTime(intervalIDFocus);
+        return;
+      }
+
       if (segundosCalculados <= 0) {
         segundosCalculados = 60;
         minutosCalculados--;
@@ -63,13 +72,86 @@ export function useTimer(props: useTimer) {
       segundosCalculados--;
       teste--;
 
+      if (segundosCalculados <= 0 && minutosCalculados <= 0) {
+        teste = focus;
+        setCiclosConcluidos((prevCiclo) => prevCiclo + 1);
+        setTempoTotal(0);
+        stopTime(intervalIDFocus);
+        shortBreakTime();
+      }
+
       setFocusSegundos(teste);
       setHoras(horasCalculadas);
       setMinutos(minutosCalculados);
       setSegundos(segundosCalculados);
     }, 1000);
 
-    setIntervalID(intervalIDTest);
+    setIntervalID(intervalIDFocus);
+  };
+
+  const shortBreakTime = () => {
+    timerFormat(shortSegundos, shortBreak, "ShortBreak");
+
+    var horasCalculadas = Math.floor(shortSegundos / 3600);
+    var minutosCalculados = Math.floor(shortSegundos / 60);
+    var segundosCalculados = shortSegundos % 60;
+    var tempoTotalCalculado = tempoTotal;
+    var teste = shortSegundos;
+
+    var intervalIDShort = setInterval(() => {
+      if (
+        !(horasCalculadas > 0) &&
+        !(minutosCalculados > 0) &&
+        !(segundosCalculados > 0)
+      ) {
+        stopTime(intervalIDShort);
+        return;
+      }
+
+      if (segundosCalculados <= 0) {
+        segundosCalculados = 60;
+        minutosCalculados--;
+      }
+
+      if (minutosCalculados <= 0 && horasCalculadas > 0) {
+        segundosCalculados = 60;
+        horasCalculadas--;
+      }
+
+      setTempoTotal((prevTempoTotal) => prevTempoTotal + 1);
+      segundosCalculados--;
+      teste--;
+
+      if (segundosCalculados <= 0 && minutosCalculados <= 0) {
+        teste = shortBreak;
+        setTempoTotal(0);
+        stopTime(intervalIDShort);
+        focusTime();
+      }
+
+      setShortSegundos(teste);
+      setHoras(horasCalculadas);
+      setMinutos(minutosCalculados);
+      setSegundos(segundosCalculados);
+    }, 1000);
+
+    setIntervalID(intervalIDShort);
+  };
+
+  const startTime = () => {
+    if (intervalID) return;
+    switch (timeBreak) {
+      case focus:
+        focusTime();
+        break;
+
+      case shortBreak:
+        shortBreakTime();
+        break;
+
+      default:
+        break;
+    }
   };
 
   const stopTime = (interval: number | undefined = intervalID) => {
