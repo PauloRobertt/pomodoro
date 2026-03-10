3ea1f0ccddb02f8c700a0d8c6809ac0dcb0e3680
paulorobertt <paulorobert5776@gmail.com>
2026-03-10 16:42:30 -0300

    feat: funções para incrementar e decrementar ao pressionar o botão "+" e "-" no menu

--- pomodoro/src/components/optionMenu/optionMenu.tsx
+++ pomodoro/src/components/optionMenu/optionMenu.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import styles from "./optionMenu.module.css";
 import stylesButton from "../../components/button/button.module.css";
@@ -9,6 +9,8 @@ import Button from "../button/button";
 
 export default function OptionMenu(props: optionMenu) {
   const [defaultValue, setDefaultValue] = useState(props.value);
+
+  const intervalRef = useRef<number>(undefined);
 
   const handleOnChange = (e: any) => {
     let value = Math.trunc(e.target.value);
@@ -21,18 +23,42 @@ export default function OptionMenu(props: optionMenu) {
     props.functionSaveConfig(props.id, defaultValue);
   }, [defaultValue]);
 
+  const inc = () => {
+    if (intervalRef.current === undefined) {
+      intervalRef.current = setInterval(inc, 100);
+    }
+    setDefaultValue((prev: number) => {
+      if (props.id !== "cycle") return prev > 60 ? (prev += 60) : prev;
+      return prev > 1 ? (prev += 1) : prev;
+    });
+  };
+
+  const dec = () => {
+    if (intervalRef.current === undefined) {
+      intervalRef.current = setInterval(dec, 100);
+    }
+
+    if (defaultValue > 0) {
+      setDefaultValue((prev: number) => {
+        if (props.id !== "cycle") return prev > 60 ? (prev -= 60) : prev;
+        return prev > 1 ? (prev -= 1) : prev;
+      });
+    }
+  };
+
+  const clear = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = undefined;
+  };
+
   return (
     <div className={styles.containerOption}>
       {props.text}
       <div className={styles.containerTimer}>
         <Button
           text="-"
-          action={() => {
-            setDefaultValue((prev: number) => {
-              if (props.id !== "cycle") return prev > 60 ? (prev -= 60) : prev;
-              return prev > 1 ? (prev -= 1) : prev;
-            });
-          }}
+          onMouseUp={clear}
+          onMouseDown={dec}
           styleButton={stylesButton.buttonTimeControl}
         />
         <div className={styles.time}>
@@ -45,14 +71,8 @@ export default function OptionMenu(props: optionMenu) {
         </div>
         <Button
           text="+"
-          action={() => {
-            setDefaultValue((prev: number) => {
-              if (prev >= 0) {
-                return props.id !== "cycle" ? (prev += 60) : (prev += 1);
-              }
-              return prev;
-            });
-          }}
+          onMouseUp={clear}
+          onMouseDown={inc}
           styleButton={stylesButton.buttonTimeControl}
         />
       </div>
