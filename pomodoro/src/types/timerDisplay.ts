f5cce8bd06c7d6d8bb654f84e96206e53a09d2b2
paulorobertt <paulorobert5776@gmail.com>
2026-04-07 18:23:32 -0300

    chore: remocao do statusTimer no type timerDisplay

--- pomodoro/src/types/timerDisplay.ts
+++ pomodoro/src/types/timerDisplay.ts
@@ -2,7 +2,6 @@ export type TimerDisplayProps = {
   horas: number;
   minutos: number;
   segundos: number;
-  statusTimer: string | null;
   timeCycle: number;
   ciclosConcluidos: number;
   tempoTotal: number;
